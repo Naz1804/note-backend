b7703aeb98126b4b1234eef4a57f3558285698f3
Naz1804 <nuhaj243@gmail.com>
2026-03-17 02:44:29 +0000

    Removed key

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: 'postgresql://neondb_owner:npg_3frGvy0xDYUB@ep-divine-water-abhatek3-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=verify-full',
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
